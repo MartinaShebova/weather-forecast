6001c8c358f53830eb75b61b57aac30634ca7cd7
Martinsita <martina2314@gmail.com>
2023-09-23 16:42:52 +0300

    Added more endpoint urls

--- src/backend-services/endpointsUrls.js
+++ src/backend-services/endpointsUrls.js
@@ -2,3 +2,4 @@ import { OPEN_WEATHER_API_KEY } from "../globals";
 
 export const GET_GEOLOCATION_BY_CITY_NAME_URL = (cityName = "London") => `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${OPEN_WEATHER_API_KEY}`;
 export const GET_5_DAYS_WEATHER_FORECAST_URL = (lat, lon) => `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`;
+export const GET_CITY_WEATHER = (cityName = "London") => `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${OPEN_WEATHER_API_KEY}&units=metric`;

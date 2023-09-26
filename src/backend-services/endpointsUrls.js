1289064dc4d240d5b84a7762145796d4b6ca43e1
Martinsita <martina2314@gmail.com>
2023-09-26 11:34:26 +0300

    Fixed api key place

--- src/backend-services/endpointsUrls.js
+++ src/backend-services/endpointsUrls.js
@@ -1,6 +1,4 @@
-import { FAVOURITE_CITY_NAME } from "../globals";
-
-const OPEN_WEATHER_API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+import { OPEN_WEATHER_API_KEY, FAVOURITE_CITY_NAME } from "../globals";
 
 export const GET_5_DAYS_WEATHER_FORECAST_URL_BY_COORDINATES = (lat, lon) => `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`;
 export const GET_5_DAYS_WEATHER_FORECAST_URL_BY_LOCATION = (cityName = FAVOURITE_CITY_NAME) => `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${OPEN_WEATHER_API_KEY}&units=metric`;
--- src/globals.js
+++ src/globals.js
@@ -1,1 +1,2 @@
+export const OPEN_WEATHER_API_KEY = "52ce27514f5547b4b3a2a160a5425605";
 export const FAVOURITE_CITY_NAME = "London";
